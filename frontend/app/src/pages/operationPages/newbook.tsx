a1c9e7a54bdfab2939b6ef1c53749093638fa209
ryan98153 <ryan98153@gmail.com>
2024-06-14 22:57:06 +0800

    restore the newbook page

--- frontend/app/src/pages/operationPages/newbook.tsx
+++ frontend/app/src/pages/operationPages/newbook.tsx
@@ -1,37 +1,10 @@
 import React, { useEffect, useState } from "react";
 
-interface itemProp {
-    id:number,
-    isbn:number,
-    author:string
-}
-
 const NewBook = () => {
-    const BookItem:itemProp = {
-        id:1,
-        isbn:2215444848,
-        author:"chen,chen-cheng"
-    }
-
-    const [BookObject, setBookObject] = useState<itemProp>({
-        id:1,
-        isbn:2215444848,
-        author:"chen,chen-cheng"
-    })
-
-    const [id, setId] = useState<number>(132)
-    const [isbn, setISBN] = useState<string>('15468489')
-    const [author, setAuthor] = useState<string>("chen,chen-cheng")
-    
 
     return (
         <div style={{padding:15}}>
-            <input
-                onChange={(e)=>{setISBN(e.target.value);console.log(e)}}
-            />
-            <div>
-                input:{isbn}
-            </div>
+            
         </div>
     );
 };
