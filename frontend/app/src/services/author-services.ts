5e833c58469cfdbb9677509b57f20ceccf423584
chester71206 <chester71206@gmail.com>
2024-06-27 17:36:21 +0800

    service: author,book,plan

--- frontend/app/src/services/author-services.ts
+++ frontend/app/src/services/author-services.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from 'axios';
 
 const API_URL = 'http://127.0.0.1:5000'
@@ -44,52 +43,4 @@ class AuthorService {
     }
 }
 
-=======
-import axios from 'axios';
-
-const API_URL = 'http://127.0.0.1:5000'
-
-class AuthorService {
-    async getAuthorInfo(
-        authorName: string
-    ) {
-        return await axios.get(API_URL + `/get_author/${authorName}`)
-    }
-
-    async addAuthor (
-        authorName: string,
-        introduction: string,
-        nationality: string,
-        birthYear: string
-    ) {
-        return await axios.post(`${API_URL}/add_author`,
-            {
-                author_name: authorName,
-                introduction: introduction,
-                nationality: nationality,
-                Birth_year: birthYear
-            }
-        )
-    }
-
-    async updateAuthorInfo(
-        authorId: string,
-        authorName: string,
-        introduction: string,
-        nationality: string,
-        birthYear: Date
-    ) {
-        return await axios.put(`${API_URL}/add_plan`,
-            {
-                author_id:authorId,
-                author_name: authorName,
-                introduction: introduction,
-                nationality: nationality,
-                Birth_year: birthYear
-            }
-        )
-    }
-}
-
->>>>>>> a89cb47486bec4bb0e07d516e86338075fb8790a
 export default new  AuthorService
--- frontend/app/src/services/plan-services.ts
+++ frontend/app/src/services/plan-services.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import axios from 'axios';
 const API_URL = 'http://127.0.0.1:5000'
 
@@ -48,55 +47,4 @@ class PlanService {
         return await axios.get(`${API_URL}/search_id_by_book_title/${book_title}`);
     }
 
-=======
-import axios from 'axios';
-const API_URL = 'http://127.0.0.1:5000'
-
-class PlanService {
-    /*
-    interface Plan {
-        bookId: number;
-        expiredDate: Date;
-        finished: boolean;
-    }
-    */
-
-    async getPlan () {
-        return await axios.get( API_URL + '/view_data/plan' )
-    }
-
-    async addPlan (
-        bookId: number,
-        expiredDate: string,
-        finished: boolean
-    ) {
-        return await axios.post(`${API_URL}/add_plan`,
-            {
-                book_id: bookId,
-                expired_date: expiredDate,
-                is_complete: finished
-            }
-        )
-    }
-
-    async deletePlan (
-        planId: number,
-    ) {
-        return await axios.delete(`${API_URL}/delete_data`, {
-            data:{
-                table: '閱讀計劃',
-                id: planId
-            }
-        })
-    }
-    async getbookname_by_id(bookId: number){
-        console.log("this_is_return:",await axios.get(`${API_URL}/search_book/${bookId}`));
-        return await axios.get(`${API_URL}/search_book/${bookId}`);
-    }
-    
-    async search_id_by_book_title(book_title:string){
-        return await axios.get(`${API_URL}/search_id_by_book_title/${book_title}`);
-    }
-
->>>>>>> a89cb47486bec4bb0e07d516e86338075fb8790a
 }export default new PlanService()
